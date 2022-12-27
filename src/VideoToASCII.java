174ef9c99ae67648808fa223ab7385f7643698cb
Wieslaw <you@example.com>
2022-12-27 15:40:28 +0100

    Creating single frames from video using ffmpeg

--- src/VideoToASCII.java
+++ src/VideoToASCII.java
@@ -1,25 +1,63 @@
 import javax.imageio.ImageIO;
 import java.io.File;
+import java.io.IOException;
 
 public class VideoToASCII {
     private ImageToASCII[] frames;
 
     public VideoToASCII(int numberOfFrames){
-       frames = new ImageToASCII[numberOfFrames];
+        frames = new ImageToASCII[numberOfFrames];
     }
 
-    public void loadImages(String fileName){
+    public void createFrames(String videoName){
+        //String[] cmd={"ffmpeg","-i",videoName,"-vf","fps=30","/home/wieslaw/Video/out%d.png"};
+        String cmd = "ffmpeg"+" -i "+videoName +" -vf " +" fps=30 "+"frames/out%d.png";
+
+        try {
+            Process p = new ProcessBuilder("/bin/sh","-c",cmd)
+                    .redirectErrorStream(true)
+                    .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                    .start();
+            p.waitFor();
+            System.out.println(p.exitValue() + p.info().toString());
+        }catch(Exception e){
+            System.out.println(e);
+        }
+
+    }
+    public void emptyDirectory(String directory){
+        String cmd = "rm " +directory+"*";
+        try {
+            Process p = new ProcessBuilder("/bin/sh","-c",cmd)
+                    .redirectErrorStream(true)
+                    .redirectOutput(ProcessBuilder.Redirect.INHERIT)
+                    .start();
+            p.waitFor();
+            System.out.println(p.exitValue() + p.info().toString());
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+
+    public void loadVideo(String videoName){
+        String outputFileName = "frames/out";
         String name;
         boolean opened;
 
+        createFrames(videoName);
+
         for(int i=0;i<frames.length;i++){
-            name=fileName+(i+1)+".png";
+            name=outputFileName+(i+1)+".png";
             frames[i] = new ImageToASCII();
             opened = frames[i].openFile(name);
+            ImageToASCII.clearScreen();
             System.out.println(name);
             //System.out.println(opened);
             frames[i].createASCII();
         }
+        emptyDirectory("frames/");
+
+
     }
     public void printImages() {
         for(int i=0;i<frames.length;i++){
--- src/VideoToASCIITest.java
+++ src/VideoToASCIITest.java
@@ -1,7 +1,7 @@
 public class VideoToASCIITest {
     public static void main(String[] arg){
         VideoToASCII video = new VideoToASCII(9600);
-        video.loadImages("/home/wieslaw/Videos/badAppleFull/out");
+        video.loadVideo("/home/wieslaw/Videos/simplescreenrecorder-2022-12-26_22.56.42.mp4");
         video.printImages();
     }
 }
